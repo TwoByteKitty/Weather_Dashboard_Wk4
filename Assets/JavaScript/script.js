c2dd3c8c6834bfa09d5e459b9dad7f1c218ba6bc
Christie Burt <cmburt@yahoo.com>
2019-10-31 00:20:27 -0400

    finished fleshing out functionality in JS. MVP - icon (couldn't get icon working)

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,33 +1,41 @@
 
+
 //api key: 570a95666fe4d4e3cacabb69009293b9
+   
+
+    //get date
+    var d = new Date();
+    var month = d.getMonth()+1;
+    var day = d.getDate();
+    
+    var currentDate = 
+        ((''+month).length<2 ? '0' : '') + month + '/' +
+        ((''+day).length<2 ? '0' : '') + day  + '/' + d.getFullYear();
+    
+
 
 function getWeather(){
     //get value of search field
+    var queryCity = $('#search').val()
 
     var APIKey = "570a95666fe4d4e3cacabb69009293b9";
-
-    // Here we are building the URL we need to query the database
+   
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-      "q=" + $('#search').val() + "&units=imperial&appid=" + APIKey;
+      "q=" + queryCity + "&units=imperial&appid=" + APIKey;
 
-    // Here we run our AJAX call to the OpenWeatherMap API
+    // Ajax call to weather api
     $.ajax({
       url: queryURL,
       method: "GET"
     })
-      // We store all of the retrieved data inside of an object called "response"
+  
       .then(function(response) {
-
         console.log(response);
-
-        $("#city").text(response.name + "|" );
+        $("#city").text(response.name + " || " + currentDate);
         $("#temp").text("Temperature (F): " + response.main.temp);
         $("#humid").text("Humidity: " + response.main.humidity);
         $("#windSp").text("Wind Speed: " + response.wind.speed);
 
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + response.main.temp);
       });
 }
 
